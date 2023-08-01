94c4a2cf69a61e375a2c679501e5ef415ed7cfbd
yangoch <yangoch.dev@gmail.com>
2023-08-01 18:18:14 +0300

    added the task "you_cannot_code_under_pressure_1"

---
+++ 8kyu/you_cannot_code_under_pressure_1.js
@@ -0,0 +1,7 @@
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i + i;
+  }
+
+  console.log(doubleInteger(2)); // 4
+  console.log(doubleInteger(8)); // 16
