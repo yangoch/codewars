25579537ccfe2d9a8c873eb6119783523510a828
yangoch <yangoch.dev@gmail.com>
2023-08-07 14:42:38 +0300

    added "MakeUpperCase"

---
+++ 8kyu/make_upper_case.js
@@ -0,0 +1,8 @@
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+console.log(makeUpperCase("")); // ""
+console.log(makeUpperCase("hello")); // "HELLO"
+console.log(makeUpperCase("Hello")); // "HELLO"
+console.log(makeUpperCase("HELLO")); // "HELLO"
