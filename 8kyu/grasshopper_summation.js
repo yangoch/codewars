a47d3d6140e8c8f93c6a1a978106cf7356622c98
yangoch <yangoch.dev@gmail.com>
2023-08-08 15:47:18 +0300

    added "Grasshopper - Summation"

---
+++ 8kyu/grasshopper_summation.js
@@ -0,0 +1,11 @@
+const summation = function (num) {
+  let sum = 0;
+  for (let i = 1; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
+};
+
+console.log(summation(1)); //  1
+console.log(summation(2)); //  3
+console.log(summation(8)); // 36
