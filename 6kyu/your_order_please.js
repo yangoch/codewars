d83e99c0568d07cbfd8614788b2ba93ec179b5d0
yangoch <yangoch.dev@gmail.com>
2023-08-11 20:53:29 +0300

    fixed 'Your Order, Please'

--- 6kyu/your_order_please.js
+++ 6kyu/your_order_please.js
@@ -16,4 +16,3 @@ function order(words) {
 console.log(order("is2 Thi1s T4est 3a")); // "Thi1s is2 3a T4est")
 console.log(order("4of Fo1r pe6ople g3ood th5e the2")); // "Fo1r the2 g3ood 4of th5e pe6ople")
 console.log(order("")); // "", "empty input should return empty string" )
-Your Order, Please
