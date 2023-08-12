589cecda2a35194319c03e77dc0254302c51d96f
yangoch <yangoch.dev@gmail.com>
2023-08-12 12:56:35 +0300

    added "Return Negative"

---
+++ 8kyu/return_negative.js
@@ -0,0 +1,8 @@
+function makeNegative(num) {
+  return num > 0 ? -num : num;
+}
+
+console.log(makeNegative(1)); // return -1
+console.log(makeNegative(-5)); // return -5
+console.log(makeNegative(0)); // return 0
+console.log(makeNegative(0.12)); // return -0.12
