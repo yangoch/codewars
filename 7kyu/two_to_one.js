b528d2c71bb5141d9ae4806ece60ed629625d2f9
yangoch <yangoch.dev@gmail.com>
2023-08-14 15:11:23 +0300

    added 'Two to One'

---
+++ 7kyu/two_to_one.js
@@ -0,0 +1,13 @@
+function longest(s1, s2) {
+  return [...new Set(s1 + s2)].sort().join("");
+}
+
+console.log(longest("aretheyhere", "yestheyarehere"), "aehrsty");
+console.log(
+  longest("loopingisfunbutdangerous", "lessdangerousthancoding"),
+  "abcdefghilnoprstu"
+);
+console.log(
+  longest("inmanylanguages", "theresapairoffunctions"),
+  "acefghilmnoprstuy"
+);
