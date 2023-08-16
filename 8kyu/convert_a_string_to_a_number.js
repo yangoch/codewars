49e73724bddf851d6ac1c829ae25aa027d54bb34
yangoch <yangoch.dev@gmail.com>
2023-08-16 13:25:49 +0300

    added 'Convert a String to a Number'

---
+++ 8kyu/convert_a_string_to_a_number.js
@@ -0,0 +1,8 @@
+const stringToNumber = function (str) {
+  return Number(str);
+};
+
+console.log(stringToNumber("1234"), 1234);
+console.log(stringToNumber("605"), 605);
+console.log(stringToNumber("1405"), 1405);
+console.log(stringToNumber("-7"), -7);
