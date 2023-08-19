bb2fa668215b11e696d6f5c17f7c497f49b8d0ad
yangoch <yangoch.dev@gmail.com>
2023-08-19 09:24:28 +0300

    added "Basic Mathematical Operations"

---
+++ 8kyu/basic_mathematical_operations.js
@@ -0,0 +1,24 @@
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+      break;
+    case "-":
+      return value1 - value2;
+      break;
+    case "*":
+      return value1 * value2;
+      break;
+    case "/":
+      return value1 / value2;
+      break;
+    default:
+      console.log("The operation is incorrect!");
+      return 0;
+  }
+}
+
+console.log(basicOp("+", 4, 7), 11);
+console.log(basicOp("-", 15, 18), -3);
+console.log(basicOp("*", 5, 5), 25);
+console.log(basicOp("/", 49, 7), 7);
