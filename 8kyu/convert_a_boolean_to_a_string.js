6aa7decb644c83ae6af597a2aa1d46bf9464d953
yangoch <yangoch.dev@gmail.com>
2023-08-30 18:48:08 +0300

    added "Convert a Boolean to a String"

---
+++ 8kyu/convert_a_boolean_to_a_string.js
@@ -0,0 +1,6 @@
+function booleanToString(b){
+  return `${b}`;
+}
+
+console.log(booleanToString(true), "true", 'When we pass in true, we want the string "true" as output');
+console.log(booleanToString(false), "false", 'When we pass in false, we want the string "false" as output');
