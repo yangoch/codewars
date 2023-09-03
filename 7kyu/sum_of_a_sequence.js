b210a693a936b65f5832f6625226312a4a0e614a
yangoch <yangoch.dev@gmail.com>
2023-09-03 18:49:35 +0300

    Sum of a sequence

---
+++ 7kyu/sum_of_a_sequence.js
@@ -0,0 +1,13 @@
+const sequenceSum = (begin, end, step) => {
+  let sum = 0;
+
+  for (let i = begin; i <= end; i += step) {
+    sum += i;
+  }
+
+  return sum;
+};
+
+console.log(sequenceSum(2, 6, 2), 12);
+console.log(sequenceSum(1, 5, 1), 15);
+console.log(sequenceSum(1, 5, 3), 5);
