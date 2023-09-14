998629b95edfadab5c3a988e90ca9a4ca54790e4
yangoch <yangoch.dev@gmail.com>
2023-09-14 21:05:17 +0300

    Count Odd Numbers below n

---
+++ 8kyu/count_odd_numbers_below_n.js
@@ -0,0 +1,20 @@
+/*
+
+8 kyu
+Count Odd Numbers below n
+
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+Examples (Input -> Output)
+7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+Expect large Inputs!
+
+*/
+
+function oddCount(n){
+  return Math.floor(n / 2);
+}
+
+console.log(oddCount(15), 7, "Oops! Wrong.");
+console.log(oddCount(15023), 7511, "Oops! Wrong.");
