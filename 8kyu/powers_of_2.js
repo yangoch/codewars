92d307f028114b25d88529f217de21f7361dd72d
yangoch <yangoch.dev@gmail.com>
2023-09-14 21:24:19 +0300

    Powers of 2

---
+++ 8kyu/powers_of_2.js
@@ -0,0 +1,21 @@
+/*
+
+8 kyu
+Powers of 2
+
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+
+*/
+
+function powersOfTwo(n) {
+  return Array.from({ length: n + 1 }, (_, index) => Math.pow(2, index));
+}
+
+console.log(powersOfTwo(0), [1]);
+console.log(powersOfTwo(1), [1, 2]);
+console.log(powersOfTwo(4), [1, 2, 4, 8, 16]);
