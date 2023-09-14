daf2de0c0fd19ce37849f3506beba43d35031d39
yangoch <yangoch.dev@gmail.com>
2023-09-14 21:13:39 +0300

    Is it a palindrome?

---
+++ 8kyu/is_it_a_palindrome.js
@@ -0,0 +1,22 @@
+/*
+
+8 kyu
+Is it a palindrome?
+
+Write a function that checks if a given string (case insensitive) is a palindrome. A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar, the date and time 12/21/33 12:21, and the sentence: "A man, a plan, a canal – Panama".
+
+*/
+
+function isPalindrome(x) {
+  const stringToLowerCase = x.toLowerCase();
+  return stringToLowerCase === stringToLowerCase.split("").reverse().join("");
+}
+
+console.log(isPalindrome("a"), true);
+console.log(isPalindrome("aba"), true);
+console.log(isPalindrome("Abba"), true);
+console.log(isPalindrome("hello"), false);
+console.log(isPalindrome("Bob"), true);
+console.log(isPalindrome("Madam"), true);
+console.log(isPalindrome("AbBa"), true);
+console.log(isPalindrome(""), true);
