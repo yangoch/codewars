710dfcd0e18da5442752bc66ebacadde80dcb97f
yangoch <yangoch.dev@gmail.com>
2023-09-16 19:51:10 +0300

    Row Weights

---
+++ 7kyu/row_weights.js
@@ -0,0 +1,54 @@
+/*
+
+7 kyu
+Row Weights
+
+Scenario
+Several people are standing in a row divided into two teams.
+The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+
+Task
+Given an array of positive integers (the weights of the people), return a new array/tuple of two integers, where the first one is the total weight of team 1, and the second one is the total weight of team 2.
+
+Notes
+Array size is at least 1.
+All numbers will be positive.
+Input >> Output Examples
+rowWeights([13, 27, 49])  ==>  return (62, 27)
+Explanation:
+The first element 62 is the total weight of team 1, and the second element 27 is the total weight of team 2.
+
+rowWeights([50, 60, 70, 80])  ==>  return (120, 140)
+Explanation:
+The first element 120 is the total weight of team 1, and the second element 140 is the total weight of team 2.
+
+rowWeights([80])  ==>  return (80, 0)
+Explanation:
+The first element 80 is the total weight of team 1, and the second element 0 is the total weight of team 2.
+
+*/
+
+function rowWeights(array) {
+  let teams = [0, 0];
+
+  array.forEach((number, index) => {
+    if(index % 2 === 0) {
+      teams[0] += number;
+    } else {
+      teams[1] += number;
+    }
+  });
+
+  return teams;
+}
+
+console.log(rowWeights([80]), [80, 0]);
+console.log(rowWeights([100, 50]), [100, 50]);
+console.log(rowWeights([50, 60, 70, 80]), [120, 140]);
+console.log(rowWeights([13, 27, 49]), [62, 27]);
+console.log(rowWeights([70, 58, 75, 34, 91]), [236, 92]);
+console.log(rowWeights([29, 83, 67, 53, 19, 28, 96]), [211, 164]);
+console.log(rowWeights([0]), [0, 0]);
+console.log(rowWeights([100, 51, 50, 100]), [150, 151]);
+console.log(rowWeights([39, 84, 74, 18, 59, 72, 35, 61]), [207, 235]);
+console.log(rowWeights([0, 1, 0]), [0, 1]);
