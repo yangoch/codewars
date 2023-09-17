ff047922d99bd6fad254cbf9b40e87466b0693e6
yangoch <yangoch.dev@gmail.com>
2023-09-17 12:36:06 +0300

    Sum of angles

---
+++ 7kyu/sum_of_angles.js
@@ -0,0 +1,15 @@
+/*
+
+7 kyu
+Sum of angles
+
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+*/
+
+function angle(n) {
+  return (n - 2) * 180;
+}
+
+console.log(angle(3), 180);
+console.log(angle(4), 360);
