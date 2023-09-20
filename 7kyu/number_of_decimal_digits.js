f19656bd37a3ad00938ea2f4c4ea508175c3c2a0
yangoch <yangoch.dev@gmail.com>
2023-09-20 14:16:48 +0300

    Number of Decimal Digits

---
+++ 7kyu/number_of_decimal_digits.js
@@ -0,0 +1,18 @@
+/*
+
+7 kyu
+Number of Decimal Digits
+
+Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+All inputs will be valid.
+
+*/
+
+function digits(n) {
+  return n.toString().length;
+}
+
+console.log(digits(5), 1, "Fail!");
+console.log(digits(12345), 5, "Fail!");
+console.log(digits(9876543210), 10, "Fail!");
