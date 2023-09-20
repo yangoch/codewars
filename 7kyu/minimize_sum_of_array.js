23251558625561eb8ce4a6a29a16627e0d2eb925
yangoch <yangoch.dev@gmail.com>
2023-09-20 14:24:54 +0300

    Minimize Sum Of Array (Array Series #1)~

---
+++ 7kyu/minimize_sum_of_array.js
@@ -0,0 +1,50 @@
+/*
+
+7 kyu
+Minimize Sum Of Array (Array Series #1)
+
+Task
+Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
+
+Notes
+Array/list will contain positives only .
+Array/list will always have even size
+Input >> Output Examples
+minSum({5,4,2,3}) ==> return (22) 
+Explanation:
+The minimum sum obtained from summing each two integers product ,  5*2 + 3*4 = 22
+minSum({12,6,10,26,3,24}) ==> return (342)
+Explanation:
+The minimum sum obtained from summing each two integers product ,  26*3 + 24*6 + 12*10 = 342
+minSum({9,2,8,7,5,4,0,6}) ==> return (74)
+Explanation:
+The minimum sum obtained from summing each two integers product ,  9*0 + 8*2 +7*4 +6*5 = 74
+
+*/
+
+function minSum(arr) {
+  const sortedArray = arr.sort((a, b) => a - b);
+  let sum = 0;
+
+  for (let i = 0; i < sortedArray.length / 2; i++) {
+    const firstElement = sortedArray[i];
+    const lastElement = sortedArray[sortedArray.length - 1 - i];
+
+    sum += firstElement * lastElement;
+  }
+
+  return sum;
+}
+
+// Best practice
+// function minSum(arr) {
+//   return arr.sort( (a,b) => a-b )
+//             .slice(0, arr.length/2)
+//             .reduce( (acc,curr,index) => acc += curr * arr[ arr.length - index - 1 ], 0 )
+// }
+// const minSum = arr =>
+//   arr.sort((a, b) => a - b).reduce((pre, val) => pre + val * arr.pop(), 0);
+
+console.log(minSum([5, 4, 2, 3]), 22);
+console.log(minSum([12, 6, 10, 26, 3, 24]), 342);
+console.log(minSum([9,2,8,7,5,4,0,6]), 74);
