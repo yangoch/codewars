e3c505a022f89708a91b06a63bf13524b8cae6fd
yangoch <yangoch.dev@gmail.com>
2023-09-27 09:22:22 +0300

    esreveR

---
+++ 7kyu/esrever.js
@@ -0,0 +1,24 @@
+/*
+
+7 kyu
+esreveR
+
+Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+(the dedicated builtin(s) functionalities are deactivated)
+
+*/
+
+reverse = function (array) {
+  let newArr = [];
+
+  for (let i = array.length - 1; i >= 0; i--) {
+    const element = array[i];
+
+    newArr.push(element);
+  }
+  return newArr;
+};
+
+console.log(reverse([1, 2, 3]), [3, 2, 1]);
+console.log(reverse([1, null, 14, "two"]), ["two", 14, null, 1]);
